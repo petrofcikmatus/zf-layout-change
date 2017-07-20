f0ae5a8a81748433700f72501f3b23ca46792da4
Matúš Petrofčík <petrofcikmatus@gmail.com>
2017-07-20 23:50:58 +0200

    Added comments to code

--- src/Module.php
+++ src/Module.php
@@ -14,31 +14,44 @@ class Module implements BootstrapListenerInterface
 {
 
     /**
-     *
+     * Config key under which this module looks for layouts to change.
      */
     const CONFIG_KEY = 'module-layouts';
 
     /**
-     * Listen to the bootstrap event
+     * Listen to the bootstrap event.
      *
      * @param EventInterface|MvcEvent $e
      * @return array
      */
     public function onBootstrap(EventInterface $e)
     {
+        // Get config array.
         $config = $e->getApplication()->getServiceManager()->get('Config');
 
-        if (isset($config[self::CONFIG_KEY])) {
+        // If in config array exists array with different module layouts...
+        if (isset($config[self::CONFIG_KEY]) && is_array($config[self::CONFIG_KEY])) {
+
+            // Get event manager.
             $eventManager = $e->getApplication()->getEventManager();
 
+            // Attach new (changing) event listener to dispatch event.
             $eventManager->attach(MvcEvent::EVENT_DISPATCH, function (EventInterface $e) use ($config) {
+
+                // Get target (controller).
                 $controller = $e->getTarget();
 
+                // Get controller class name.
                 $controllerClass = get_class($controller);
-                $moduleNamespace = substr($controllerClass, 0, strpos($controllerClass, '\\'));
 
-                if (isset($config[self::CONFIG_KEY][$moduleNamespace])) {
-                    $controller->layout($config[self::CONFIG_KEY][$moduleNamespace]);
+                // Get controller namespace from controller class name.
+                $controllerNamespace = substr($controllerClass, 0, strpos($controllerClass, '\\'));
+
+                // If there is layout (key) in config array matching controller namespace...
+                if (isset($config[self::CONFIG_KEY][$controllerNamespace])) {
+
+                    // Set (change) layout.
+                    $controller->layout($config[self::CONFIG_KEY][$controllerNamespace]);
                 }
             });
         }
